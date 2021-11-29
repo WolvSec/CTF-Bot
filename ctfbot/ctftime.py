847cce994f08b4e1f0f3f4d7024985070523e63b
qhdwight <qhdwight@gmail.com>
2021-11-29 14:01:35 -0500

    Move to pycord, delete IDE files, change to a module, add slash commands

--- ctfbot/ctftime.py
+++ ctfbot/ctftime.py
@@ -8,10 +8,10 @@ HEADERS = {
     'User-Agent': 'WolvSec Discord Bot/0.1.0',
 }
 
-BASE_URL = f'https://ctftime.org/api/v1/events/'
+BASE_URL = 'https://ctftime.org/api/v1'
 
 
-def wrap_response(response):
+def _wrap_response(response):
     if not response.ok:
         raise HTTPError()
     return json.loads(response.content)
@@ -24,11 +24,16 @@ def get_upcoming(start: datetime.datetime = None, finish: datetime.datetime = No
         raise Exception('Start must be before end')
     start_timestamp = int(start.timestamp())
     finish_timestamp = int(finish.timestamp())
-    response = requests.get(f'{BASE_URL}?limit={limit}&start={start_timestamp}&finish={finish_timestamp}',
+    response = requests.get(f'{BASE_URL}/events/?limit={limit}&start={start_timestamp}&finish={finish_timestamp}',
                             headers=HEADERS)
-    return wrap_response(response)
+    return _wrap_response(response)
 
 
 def get_event(event_id: int) -> dict:
-    response = requests.get(f'{BASE_URL}{event_id}/', headers=HEADERS)
-    return wrap_response(response)
+    response = requests.get(f'{BASE_URL}/events/{event_id}/', headers=HEADERS)
+    return _wrap_response(response)
+
+
+def get_team(team_id: int) -> dict:
+    response = requests.get(f'{BASE_URL}/teams/{team_id}/', headers=HEADERS)
+    return _wrap_response(response)
