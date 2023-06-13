f6bef6fa10c7ffc1cb6ffd48f3666b918ec9cb93
dreeSec <74334127+dreeSec@users.noreply.github.com>
2023-06-12 22:48:21 -0400

    added ctf channel manager

--- ctfbot/cog.py
+++ ctfbot/cog.py
@@ -17,7 +17,7 @@ def iso_to_pretty(iso):
     return datetime.fromisoformat(iso).strftime("%B %d at %I%p")
 
 
-class CtfCog(commands.Cog):
+class CtfCog(commands.Cog):        
     data: GlobalData = None
 
     @staticmethod
@@ -32,8 +32,9 @@ class CtfCog(commands.Cog):
             embed.add_field(name='Weight', value=event['weight'])
         return embed
 
-    def __init__(self):
+    def __init__(self, bot):
         self.load_data()
+        self.bot = bot
         # self.scheduler = sched.scheduler(datetime.utcnow, time.sleep)
 
     @commands.Cog.listener()
@@ -83,7 +84,7 @@ class CtfCog(commands.Cog):
     @commands.slash_command()
     @commands.has_permissions(administrator=True)
     async def register(self, ctx: discord.ApplicationContext,
-                       event_id: discord.Option(int), channel_name: discord.Option(str)):
+                       event_id: discord.Option(int), category_name: discord.Option(str)):
         data = self.data.servers[ctx.guild_id]
         if str(event_id) in data.events or str(event_id) in data.archived_events:
             await ctx.respond('You have already registered/played this event!')
@@ -93,11 +94,31 @@ class CtfCog(commands.Cog):
             await ctx.respond('Event not found')
             return
         guild: discord.Guild = ctx.guild
-        category: discord.CategoryChannel = guild.get_channel(config('CTF_CATEGORY_ID', cast=int))
-        channel: discord.TextChannel = await guild.create_text_channel(name=channel_name, category=category)
-        message: discord.Message = await channel.send(embed=self.create_event_embed(event))
-        await message.pin()
-        data.events[event_id] = channel.id
+        category: discord.CategoryChannel = await guild.create_category(name=category_name, position=config('CTF_CATEGORY_POS'))
+        data.events[event_id] = category.id
+        overwrites = {guild.default_role: discord.PermissionOverwrite(send_messages=False, add_reactions=False, manage_threads=False)}
+        channel_join: discord.TextChannel = await guild.create_text_channel(name='join-ctf', category=category, overwrites=overwrites)
+        overwrites = {guild.default_role: discord.PermissionOverwrite(read_messages=False)}
+        channel_join_logs: discord.TextChannel = await guild.create_text_channel(name='logs', category=category, overwrites=overwrites)
+        channel_challenges: discord.TextChannel = await guild.create_text_channel(name='challenges', category=category, overwrites=overwrites)
+        channel_general: discord.TextChannel = await guild.create_text_channel(name='general', category=category, overwrites=overwrites)
+        message_challenges_embed = discord.Embed(
+        title=category_name + " Challenges",
+        description=f"Current and solved challenges. To add a challenge, use the command /challenge in {channel_general.mention}. To solve a challenge, use the command /solve in a challenge thread. If you created a challenge by mistake, contact an admin to use /remove",
+        color=discord.Colour.yellow(),
+        )
+        message_challenges: discord.Message = await channel_challenges.send(embed=message_challenges_embed)
+        message_info: discord.Message = await channel_join.send(embed=self.create_event_embed(event))
+        message_join_embed = discord.Embed(
+        title="Join " + category_name,
+        description="To join the CTF, react with the :white_check_mark: emoji below!" +
+        " You must have the CTF-Verified role to join. Please message an admin/officer to obtain this role.",
+        color=discord.Colour.green(),
+        )
+        message_join: discord.Message = await channel_join.send(embed=message_join_embed)
+        await message_join.add_reaction("✅")
+        await ctx.respond(f"Event Created! Join at {channel_join.mention}")
+
         self.write_data()
 
     @commands.slash_command()
@@ -147,3 +168,164 @@ class CtfCog(commands.Cog):
         else:
             await ctx.respond('Added reminder for this event')
             del data.reminders[ctx.channel_id]
+    
+
+    @commands.slash_command()
+    async def challenge(self, ctx: discord.ApplicationContext, chal_category: discord.Option(str), chal_name: discord.Option(str)):
+        if '→' in chal_category or '→' in chal_name or '**' in chal_category or '**' in chal_name or '~~' in chal_category or '~~' in chal_name:
+            await ctx.respond('Invalid character in challenge name/category')
+            return
+        guild: discord.Guild = ctx.guild
+        data = self.data.servers[ctx.guild_id]
+        for event_id, category_id in data.events.items():
+            category = guild.get_channel(category_id)
+            if category and ctx.channel_id == category.channels[3].id:
+                break
+        else:
+            await ctx.respond('This is not a CTF channel')
+            return
+        channel_challenges: discord.TextChannel = category.channels[2]
+        channel_general: discord.TextChannel = category.channels[3]
+        message_challenges: discord.Message = await channel_challenges.fetch_message(channel_challenges.last_message_id)
+        embed = message_challenges.embeds[0]
+        thread = None
+        for index, field in enumerate(embed.fields):
+            if field.name == f'**{chal_category}**':
+                for i in range(index + 1, len(embed.fields)):
+                    if embed.fields[i].value.startswith(chal_name):
+                        await ctx.respond('Challenge already exists')
+                        return
+                    elif embed.fields[i].name.startswith('**'):
+                        thread = await channel_general.create_thread(name=chal_category + '/' + chal_name, type=discord.ChannelType.public_thread)
+                        embed.insert_field_at(i, name='', value=chal_name + ' → ' + thread.mention, inline=False)
+                        break
+                else:
+                    thread = await channel_general.create_thread(name=chal_category + '/' + chal_name, type=discord.ChannelType.public_thread)
+                    embed.add_field(name='', value=chal_name + ' → ' + thread.mention, inline=False)
+                break
+        else:
+            thread = await channel_general.create_thread(name=chal_category + '/' + chal_name, type=discord.ChannelType.public_thread)
+            embed.add_field(name=f'**{chal_category}**', value='')
+            embed.add_field(name='', value=chal_name + ' → ' + thread.mention, inline=False)
+        await message_challenges.edit(embed=embed)
+        await ctx.respond(f'Challenge created {thread.mention}')
+
+    @commands.slash_command()
+    @commands.has_permissions(administrator=True)
+    async def remove(self, ctx: discord.ApplicationContext, chal_category: discord.Option(str), chal_name: discord.Option(str)):
+        guild: discord.Guild = ctx.guild
+        data = self.data.servers[ctx.guild_id]
+        for event_id, category_id in data.events.items():
+            category = guild.get_channel(category_id)
+            if category and ctx.channel_id == category.channels[3].id:
+                break
+        else:
+            await ctx.respond('This is not a CTF channel')
+            return
+        channel_challenges: discord.TextChannel = category.channels[2]
+        message_challenges: discord.Message = await channel_challenges.fetch_message(channel_challenges.last_message_id)
+        embed = message_challenges.embeds[0]
+        for index, field in enumerate(embed.fields):
+            if field.name == f'**{chal_category}**':
+                for i in range(index + 1, len(embed.fields)):
+                    if embed.fields[i].value.startswith(chal_name):
+                        thread_id = int(embed.fields[i].value.split(' → <#')[1].split('>')[0])
+                        thread: discord.Thread = guild.get_thread(thread_id)
+                        await thread.delete()
+                        embed.remove_field(i)
+                        if embed.fields[i - 1].name.startswith('**') and (i == len(embed.fields) or embed.fields[i].name.startswith('**')):
+                            embed.remove_field(i - 1)
+                        await message_challenges.edit(embed=embed)
+                        await ctx.respond('Challenge removed')
+                        return
+        await ctx.respond('Challenge not found')
+        
+    @commands.slash_command()
+    async def solve(self, ctx: discord.ApplicationContext, flag: discord.Option(str), display_flag: discord.Option(bool)):
+        guild: discord.Guild = ctx.guild
+        data = self.data.servers[ctx.guild_id]
+        found_thread = False
+        for event_id, category_id in data.events.items():
+            category = guild.get_channel(category_id)
+            channel_challenges: discord.TextChannel = category.channels[2]
+            message_challenges: discord.Message = await channel_challenges.fetch_message(channel_challenges.last_message_id)
+            embed = message_challenges.embeds[0]
+            for index, field in enumerate(embed.fields):
+                if not field.name.startswith('**'):
+                    thread_id = int(field.value.split(' → <#')[1].split('>')[0])
+                    if (ctx.channel_id == thread_id):
+                        embed_field = embed.fields[index]
+                        found_thread = True
+                        break
+            if found_thread:
+                break
+        else:
+            await ctx.respond('This is not a CTF thread')
+            return
+        if (embed_field.value.startswith('~~')):
+            await ctx.respond('Challenge already solved')
+            return
+        challenge_name = embed_field.value.split(' → ')[0]
+        if not display_flag:
+            flag = "HIDDEN"
+        message_challenges_embed = discord.Embed(
+        title=f"{challenge_name} has been solved!",
+        description=f"{ctx.author.mention} has solved with flag: {flag}",
+        color=discord.Colour.green(),
+        )
+        message_solve: discord.Message = await category.channels[3].send(embed=message_challenges_embed)
+        embed.set_field_at(index, name='', value="~~" + embed_field.value + '~~ has been solved by ' + ctx.author.mention + '!', inline=False)
+        await message_challenges.edit(embed=embed)
+        await ctx.respond('Flag submitted!')
+        
+
+    # For testing only
+    # @commands.slash_command()
+    # @commands.has_permissions(administrator=True)
+    # async def cleanup(self, ctx: discord.ApplicationContext):
+    #     guild: discord.Guild = ctx.guild
+    #     command_channel: discord.TextChannel = guild.get_channel(ctx.channel_id)
+    #     for category in guild.categories:
+    #         for channel in category.channels:
+    #             if channel != command_channel:
+    #                 await channel.delete()
+    #         await category.delete()
+    #     await ctx.respond('Done!')
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        if payload.member.bot:
+            return
+        guild = self.bot.get_guild(payload.guild_id)
+        data = self.data.servers[guild.id]
+        for event_id, category_id in data.events.items():
+            category = guild.get_channel(category_id)
+            if category and payload.channel_id == category.channels[0].id:
+                break
+        else:
+            return
+        player = await guild.fetch_member(payload.user_id)
+        if discord.utils.get(guild.roles, id=config('CTF_VERIFIED_ROLE_ID', cast=int)) not in player.roles:
+            player = await bot.fetch_user(payload.user_id)
+            await player.send('You do not have the CTF-Verified role! Please contact an admin to get this role.')
+            return
+        await category.channels[1].set_permissions(player, read_messages=True, send_messages=False, add_reactions=False, manage_threads=False)
+        await category.channels[2].set_permissions(player, read_messages=True, send_messages=False, add_reactions=False, manage_threads=False)
+        await category.channels[3].set_permissions(player, read_messages=True)
+        await category.channels[1].send(f'{player.mention} has joined the CTF!')
+    
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        guild = self.bot.get_guild(payload.guild_id)
+        data = self.data.servers[guild.id]
+        for event_id, category_id in data.events.items():
+            category = guild.get_channel(category_id)
+            if category and payload.channel_id == category.channels[0].id:
+                break
+        else:
+            return
+        player = await guild.fetch_member(payload.user_id)
+        await category.channels[1].set_permissions(player, read_messages=False)
+        await category.channels[2].set_permissions(player, read_messages=False)
+        await category.channels[3].set_permissions(player, read_messages=False)
+        await category.channels[1].send(f'{player.mention} has left the CTF!')
